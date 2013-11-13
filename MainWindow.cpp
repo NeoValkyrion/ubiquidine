839dd9533f2943a2fd3772c81fa70321f5dd11ad
Eric Crawford <liquidman9@gmail.com>
2013-11-12 19:07:47 -0800

    added MTPlateDetector. We can now sorta tell if there is an empty plate.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -11,6 +11,8 @@
 #include "opencv2/nonfree/nonfree.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
+#include "MTPlateDetector.h"
+#include <iostream>
 
 using namespace cv;
 using namespace Microsoft::KinectBridge;
@@ -443,7 +445,8 @@ DWORD WINAPI CMainWindow::ProcessThread()
         numEvents = 1;
     }
 
-	string s1 ="photo.JPG";
+	string s1 ="photo1.JPG";
+	MTPlateDetector platedetector(s1);
 	Mat img_object = imread( s1, CV_LOAD_IMAGE_GRAYSCALE );
 	int minHessian = 500;
 
@@ -552,75 +555,9 @@ DWORD WINAPI CMainWindow::ProcessThread()
                     continue;
                 }
 
-				detector.detect( m_colorMat, keypoints_scene );
-				extractor.compute( m_colorMat, keypoints_scene, descriptors_scene );
-				FlannBasedMatcher matcher;
-				std::vector< DMatch > matches;
-				matches.reserve(10000);
-				matcher.match( descriptors_object, descriptors_scene, matches );
-
-				double max_dist = 0; double min_dist = 100;
-
-				//-- Quick calculation of max and min distances between keypoints
-				for( int i = 0; i < descriptors_object.rows; i++ )
-				{ double dist = matches[i].distance;
-				if( dist < min_dist ) min_dist = dist;
-				if( dist > max_dist ) max_dist = dist;
+				if(platedetector.detect(m_colorMat)) {
+					std::cout << "I SEE DAT MOTHERFUCKER" << endl;		
 				}
-
-				//-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
-				std::vector< DMatch > good_matches;
-				good_matches.reserve(keypoints_scene.size() + keypoints_object.size());
-
-
-				//matcher.knnMatch(des_object, des_image, matches, 2);
-
-				//for(int i = 0; i < min(des_image.rows-1,(int) matches.size()); i++) //THIS LOOP IS SENSITIVE TO SEGFAULTS
-				//{
-				//	if((matches[i][0].distance < 0.6*(matches[i][1].distance)) && ((int) matches[i].size()<=2 && (int) matches[i].size()>0))
-				//	{
-				//		good_matches.push_back(matches[i][0]);
-				//	}
-				//}
-
-
-				for( int i = 0; i < descriptors_object.rows; i++ )
-				{ if(matches[i].distance < 3*min_dist )
-				{ good_matches.push_back( matches[i]); }
-				}
-
-				std::vector<Point2f> obj;
-				std::vector<Point2f> scene;
-
-				for( int i = 0; i < good_matches.size(); i++ )
-				{
-					//-- Get the keypoints from the good matches
-					obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
-					scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
-				}
-
-				if(good_matches.size() > 0) H = findHomography( obj, scene, CV_RANSAC );
-
-				 drawMatches( img_object, keypoints_object, m_colorMat, keypoints_scene,
-							good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-							vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
-
-				////-- Get the corners from the image_1 ( the object to be "detected" )
-				std::vector<Point2f> obj_corners(4);
-				obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
-				obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
-				std::vector<Point2f> scene_corners(4);
-				if(good_matches.size() > 0)	perspectiveTransform( obj_corners, scene_corners, H);
-
-				////-- Draw lines between the corners (the mapped object in the scene - image_2 )
-				line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
-				line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-				line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-				line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-
-				imshow( "Good Matches & Object detection", img_matches);
-				waitKey(0);
 
 				// Apply filter to color stream
 				hr = m_openCVHelper.ApplyColorFilter(&m_colorMat);
