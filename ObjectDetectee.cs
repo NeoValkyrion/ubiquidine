4fecc5306cf7264426c76a712e4bb7a9531b1614
Eric Crawford <liquidman9@gmail.com>
2013-11-17 19:16:43 -0800

    cleaned up object detection so we can detect multiple objects. Messed with object detection algorithm as well. Added waiter calling test object.

---
+++ ObjectDetectee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.GPU;
+
+namespace FaceTrackingBasics
+{
+    public class ObjectDetectee
+    {
+        private Image<Gray, Byte> _objectImage;
+        public Image<Gray, Byte> objectImage
+        {
+            get { return _objectImage; }
+            set { _objectImage = value; }
+        }
+
+        private VectorOfKeyPoint _objectKeyPoints;
+        public VectorOfKeyPoint objectKeyPoints
+        {
+            get { return _objectKeyPoints; }
+            set { _objectKeyPoints = value; }
+        }
+        private Matrix<float> _objectDescriptors;
+        
+        public Matrix<float> objectDescriptors
+        {
+            get { return _objectDescriptors; }
+            set { _objectDescriptors = value; }
+        }
+
+        private void extractData() {
+             SURFDetector surfCPU = new SURFDetector(500, false);
+
+            _objectKeyPoints = surfCPU.DetectKeyPointsRaw(objectImage, null);
+            _objectDescriptors = surfCPU.ComputeDescriptorsRaw(objectImage, null, objectKeyPoints);
+        }
+
+        public ObjectDetectee(Image<Gray,Byte> image) {
+            _objectImage = image;
+            extractData();
+        }
+
+        public ObjectDetectee(string filename)
+        {
+            _objectImage = new Image<Gray, Byte>(filename);
+            extractData();           
+        }
+    }
+}
